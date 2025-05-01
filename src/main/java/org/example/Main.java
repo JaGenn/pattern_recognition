c20f91b1833277ca80cb8f8f1021e059995a06a6
JaGenn <dzhan.dzhan.2000@mail.ru>
2025-05-01 16:57:34 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,13 @@
 package org.example;
 
-
 import org.example.letter.CompareLauncher;
-
-
-import java.util.Arrays;
 
 public class Main {
 
     private static final CompareLauncher compareLauncher = new CompareLauncher();
 
     public static void main(String[] args) {
-
+        compareLauncher.init();
     }
 
 }
